439f2da249d93d9240357428a47d6fa8f59d4fee
Johannes Bader <baderj@gmx.net>
2015-12-17 10:36:38 +0100

    Third word list, provided by Sándor Nemes. He extracted the word list from this sample:
    https://www.virustotal.com/en/file/4ee8484b95d924fe032feb8f26a44796f37fb45eca3593ab533a06785c6da8f8/analysis/

--- suppobox/dga.py
+++ suppobox/dga.py
@@ -3,7 +3,11 @@
     - https://www.endgame.com/blog/malware-with-a-personal-touch.html
     - http://www.rsaconference.com/writable/presentations/file_upload/br-r01-end-to-end-analysis-of-a-domain-generating-algorithm-malware-family.pdf 
 
-    requires words1.txt and words2.txt
+    requires words1.txt, words2.txt and words3.txt
+
+    Thanks to Sándor Nemes who provided the third wordlist. It is taken
+    from this sample:
+    https://www.virustotal.com/en/file/4ee8484b95d924fe032feb8f26a44796f37fb45eca3593ab533a06785c6da8f8/analysis/
 """
 import time
 from datetime import datetime
@@ -44,7 +48,7 @@ def generate_domains(time_, word_list):
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     datefmt = "%Y-%m-%d %H:%M:%S"
-    parser.add_argument('set', choices=[1,2], type=int, help="word list")
+    parser.add_argument('set', choices=[1,2,3], type=int, help="word list")
     parser.add_argument('-t', '--time', 
             help="time (default is now: %(default)s)",
             default=datetime.now().strftime(datefmt))
