96dfc6be964b47fa911cdcb672e3cb84a5d19b2e
Johannes Bader <baderj@gmx.net>
2017-09-27 13:19:40 +0200

    Ramdo

---
+++ ramdo/dga.py
@@ -0,0 +1,16 @@
+def dga(seed, nr):
+    s = (2 * seed * (nr + 1))
+    r = s ^ (26 * seed * nr)
+    domain = ""
+    for i in xrange(16):
+        r = r & 0xFFFFFFFF
+        domain += chr(r % 26 + ord('a'))
+        r += (r ^ (s*i**2*26))
+ 
+    domain += ".org"
+    return domain
+
+for nr in range(20):
+    print(dga(0xD5FFF, nr))
+
+
