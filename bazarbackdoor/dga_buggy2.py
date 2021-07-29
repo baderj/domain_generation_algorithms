dea2b124aadcf1f3ef7d311b8b69f7a515a4ba90
Johannes Bader <baderj@gmx.net>
2021-07-29 22:53:11 +0200

    new BazarLoader DGA (again a buggy version)

---
+++ bazarbackdoor/dga_buggy2.py
@@ -0,0 +1,53 @@
+from itertools import product
+from datetime import datetime
+import argparse
+from collections import namedtuple
+
+Param = namedtuple('Param', 'mul mod idx')
+pool = (
+    "yzewevmeywreomviekwyavygontowaerudsoyrexvuamtyseweesuvizpituiqow"
+    "uzoretzemuultiazicukoqiwolxuykosupwiymitisneroxeyxanlekyixxirasi"
+    "asxoapuxqaoh"
+)
+
+pool +=(361*2 - len(pool))*"?"
+
+
+
+def dga(date):
+    seed = date.strftime("%m%Y")
+    params = [
+        Param(19, 19, 0),
+        Param(19, 19, 1),
+        Param(4, 22, 4),
+        Param(4, 4, 5)
+    ]
+
+    ranges = []
+    for p in params:
+        s = int(seed[p.idx])
+        lower = p.mul*s
+        upper = lower + p.mod
+        ranges.append(list(range(lower, upper)))
+
+    domains = set()
+    for indices in product(*ranges):
+        domain = ""
+        for index in indices:
+            domain += pool[index*2:index*2 + 2]
+        domain += ".bazar"
+        domains.add(domain)
+
+    return domains
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d", "--date", help="date used for seeding, e.g., 2020-06-28",
+        default=datetime.now().strftime('%Y-%m-%d'))
+    args = parser.parse_args()
+    d = datetime.strptime(args.date, "%Y-%m-%d")
+    for domain in dga(d):
+        print(domain)
