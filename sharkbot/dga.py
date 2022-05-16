4975513f3ee21fdd8d181bc1cde5fd6dbe268b04
Johannes Bader <git@bin.re>
2022-05-16 14:15:42 +0200

    added older versions, accesible with -v cli argument

--- sharkbot/dga.py
+++ sharkbot/dga.py
@@ -1,9 +1,10 @@
 import argparse
 import hashlib
+import base64
 from datetime import datetime
 
 
-def dga(date: datetime):
+def dga(date: datetime, version: str):
     tlds = [".xyz", ".live", ".com", ".store", ".info", ".top", ".net"]
 
     # this will actually be locale dependent, so be prepared for + or -
@@ -11,9 +12,20 @@ def dga(date: datetime):
     week_of_year = date.isocalendar()[1]
     year = date.year
     for tld in tlds:
-        s = f"{tld}{week_of_year}{year}"
-        h = hashlib.md5(s.encode("ascii")).hexdigest()
-        yield f"{h[:16]}{tld}"
+        if version == "2.8":
+            s = f"{tld}{week_of_year}{year}"
+            h = hashlib.md5(s.encode("ascii")).hexdigest()
+            sld = h[:16]
+        elif version == "2.1":
+            s = f"{tld}{week_of_year}"
+            h = hashlib.md5(s.encode("ascii")).hexdigest()
+            t = f"{h}{year}"
+            sld = t[:16]
+        elif version == "1.0":
+            s = f"{week_of_year}pojBI9LHGFdfgegjjsJ99hvVGHVOjhksdf"
+            b = base64.b64encode(s.encode('ascii'))
+            sld = b[:19].decode('ascii').lower()
+        yield f"{sld}{tld}"
 
 
 if __name__ == "__main__":
@@ -21,11 +33,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "-d", "--date", help="date for which to generate domains, e.g., 2022-05-09"
     )
+    parser.add_argument(
+        "-v", "--version", help="version of the dga", choices=["1.0", "2.8", "2.1"], default="2.8"
+    )
     args = parser.parse_args()
     if args.date:
         date = datetime.strptime(args.date, "%Y-%m-%d")
     else:
         date = datetime.now()
 
-    for domain in dga(date=date):
+    for domain in dga(date=date, version=args.version):
         print(domain)
